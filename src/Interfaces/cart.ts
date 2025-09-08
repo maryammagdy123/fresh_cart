eb8c97d9caf535ee57d50980c5f227927fdc44e2
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-09 00:40:02 +0300

    `feat: updated cart interface and page to include product details and quantity controls`

--- src/Interfaces/cart.ts
+++ src/Interfaces/cart.ts
@@ -1,24 +1,40 @@
+
+import { Brand } from "./brand";
+import { Category, Subcategory } from "./category";
+
 export interface CartResponse {
-	status: string
-	message: string
-	numOfCartItems: number
-	cartId: string
-	data: CartData
+	status: string,
+	message: string,
+	numOfCartItems: number,
+	cartId: string,
+	data: CartData,
 }
 
 interface CartData {
-	_id: string
-	cartOwner: string
-	products: CartProduct[]
-	createdAt: string
-	updatedAt: string
-	__v: number
-	totalCartPrice: number
+	_id: string,
+	cartOwner: string,
+	products: CartProduct[],
+	createdAt: string,
+	updatedAt: string,
+	__v: number,
+	totalCartPrice: number,
 }
 
 interface CartProduct {
-	count: number
-	_id: string
-	product: string
-	price: number
+	count: number,
+	_id: string,
+	product: string | Product,
+	price: number,
+}
+
+export interface Product {
+	subcategory: Subcategory[];
+	_id: string;
+	id?: string;
+	title: string;
+	quantity: number;
+	imageCover?: string;
+	category: Category;
+	brand: Brand;
+	ratingsAverage?: number;
 }
--- src/app/(Pages)/cart/page.tsx
+++ src/app/(Pages)/cart/page.tsx
@@ -2,11 +2,16 @@
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import { Trash2, Plus, Minus } from "lucide-react"
+import { apiServices } from "@/services/api"
+import { useState } from "react"
+import { CartResponse } from "@/Interfaces"
 
 export default function Cart() {
-
-	async function handleGetUserCart(params: type) {
-
+	const [cart, setCart] = useState(null)
+	async function handleGetUserCart() {
+		const data = await apiServices.getUserCart()
+		console.log(data.message)
+		setc
 	}
 
 	return (
@@ -17,60 +22,62 @@ export default function Cart() {
 				{/* products list */}
 				<div className="lg:col-span-2 space-y-4">
 
-					<div
-						// key={item.id}
-						className="flex gap-4 p-4 rounded-2xl border bg-white shadow-sm hover:shadow-md transition"
-					>
-						{/* product image */}
-						<div className="relative w-28 h-28 shrink-0">
-							{/* <Image
+					{cart.map((item) => {
+						<div
+							// key={item.id}
+							className="flex gap-4 p-4 rounded-2xl border bg-white shadow-sm hover:shadow-md transition"
+						>
+							{/* product image */}
+							<div className="relative w-28 h-28 shrink-0">
+								{/* <Image
 									src={item.image}
 									alt={item.title}
 									fill
 									className="object-cover rounded-xl"
 								/> */}
-						</div>
-
-						{/* product info */}
-						<div className="flex-1 flex flex-col justify-between">
-							<div>
-								<h2 className="font-semibold text-lg">item.title</h2>
-								<p className="text-sm text-gray-500">item.category</p>
-								<p className="text-primary font-bold mt-1">$item price</p>
 							</div>
 
-							{/* quantity controls */}
-							<div className="flex items-center gap-3 mt-3">
+							{/* product info */}
+							<div className="flex-1 flex flex-col justify-between">
+								<div>
+									<h2 className="font-semibold text-lg">item.title</h2>
+									<p className="text-sm text-gray-500">item.category</p>
+									<p className="text-primary font-bold mt-1">$item price</p>
+								</div>
+
+								{/* quantity controls */}
+								<div className="flex items-center gap-3 mt-3">
+									<Button
+										variant="outline"
+										size="icon"
+
+									>
+										<Minus className="h-4 w-4" />
+									</Button>
+									<span className="w-6 text-center">item.qty</span>
+									<Button
+										variant="outline"
+										size="icon"
+
+									>
+										<Plus className="h-4 w-4" />
+									</Button>
+								</div>
+							</div>
+
+							{/* right side: total + remove */}
+							<div className="flex flex-col justify-between items-end">
+								<p className="font-semibold">$item.price * item.qty</p>
 								<Button
-									variant="outline"
+									variant="destructive"
 									size="icon"
 
 								>
-									<Minus className="h-4 w-4" />
-								</Button>
-								<span className="w-6 text-center">item.qty</span>
-								<Button
-									variant="outline"
-									size="icon"
-
-								>
-									<Plus className="h-4 w-4" />
+									<Trash2 className="h-4 w-4" />
 								</Button>
 							</div>
 						</div>
-
-						{/* right side: total + remove */}
-						<div className="flex flex-col justify-between items-end">
-							<p className="font-semibold">$item.price * item.qty</p>
-							<Button
-								variant="destructive"
-								size="icon"
-
-							>
-								<Trash2 className="h-4 w-4" />
-							</Button>
-						</div>
-					</div>
+					})}
 
 				</div>
 
