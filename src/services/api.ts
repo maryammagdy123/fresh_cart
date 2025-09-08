b5a2ff7b0a65814856d52c9fd4daac986b5202dd
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-08 17:57:52 +0300

    `Added getHeaders method to fetch API requests with token and content type headers`

--- src/services/api.ts
+++ src/services/api.ts
@@ -42,16 +42,20 @@ class ApiServices {
 		}).then((res) => res.json());
 	}
 
+	// headers
+	getHeaders() {
+		return {
+			"Content-Type": "application / json",
+			token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YmM5NmM3YWY5NTQzMDZiMTAxNTQ1NSIsIm5hbWUiOiJNYXJpYW0gTWFnZHkiLCJyb2xlIjoidXNlciIsImlhdCI6MTc1NzM0Mjg1MywiZXhwIjoxNzY1MTE4ODUzfQ.HqAEbvRX4OO1NLocoCBgdcmXcUO-vSvQ4FtBZNUajVA"
+		}
+	}
+
 	// add products to cart
 	async addToCart(productId: string | string[]) {
 		return fetch(`https://ecommerce.routemisr.com/api/v1/cart`, {
 			method: 'post',
 			body: JSON.stringify({ productId }),
-			headers: {
-				"Content-Type": "application / json",
-				token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YmM5NmM3YWY5NTQzMDZiMTAxNTQ1NSIsIm5hbWUiOiJNYXJpYW0gTWFnZHkiLCJyb2xlIjoidXNlciIsImlhdCI6MTc1NzM0Mjg1MywiZXhwIjoxNzY1MTE4ODUzfQ.HqAEbvRX4OO1NLocoCBgdcmXcUO-vSvQ4FtBZNUajVA"
-			}
-
+			headers: this.getHeaders()
 		})
 
 	}
