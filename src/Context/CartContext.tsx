77a0bc8297a17250708ae5732f1931845a521240
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-11 01:59:59 +0300

    feat: enhance CartContext to manage cartCount and loading state

--- src/Context/CartContext.tsx
+++ src/Context/CartContext.tsx
@@ -1,11 +1,30 @@
-import { createContext, ReactNode } from "react";
+import { apiServices } from '@/services/api';
+import React, { Dispatch, SetStateAction, useEffect } from 'react'
+import { createContext, ReactNode, useState } from "react";
 
-export const cartContext = createContext(0)
-import React from 'react'
+export const cartContext = createContext<{ cartCount: number; setCartCount: Dispatch<SetStateAction<number>>, isLoading: boolean }>(({
+	cartCount: 0,
+	setCartCount: () => { },
+	isLoading: true
+})
+)
 
 export default function CartContextProvider({ children }: { children: ReactNode }) {
+	const [cartCount, setCartCount] = useState(0)
+	const [isLoading, setIsLoading] = useState(true)
+
+	async function getCart() {
+		setIsLoading(true)
+		const res = await apiServices.getUserCart()
+		setCartCount(res.numOfCartItems)
+		setIsLoading(false)
+	}
+
+	useEffect(() => {
+		getCart()
+	}, [])
 
 	return (
-		<cartContext.Provider value={{}}>{children}</cartContext.Provider>
+		<cartContext.Provider value={{ cartCount, setCartCount, isLoading }}>{children}</cartContext.Provider>
 	)
 }
