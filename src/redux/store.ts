07328c72e6082faed793255cb071478249b93962
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-14 18:50:07 +0300

    `Added CounterReducer to store configuration`

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { CounterReducer } from "./slices/CounterSlice";
 
 export const store = configureStore({
-	reducer: {}
+	reducer: { counter: CounterReducer }
 })
