80e4dfe7b01b504237f58ead946ab50da8188302
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-14 18:17:50 +0300

    `Added @reduxjs/toolkit and react-redux dependencies`

---
+++ src/components/StoreProvider/ProvidersContainer.tsx
@@ -0,0 +1,8 @@
+"use client"
+import React, { ReactNode } from 'react'
+
+export default function ProvidersContainer({ children }: { children: ReactNode }) {
+	return (
+		<div>{children}</div>
+	)
+}
