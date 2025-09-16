92ef51434ffaf2f9bac106b311975f4da3ddc0f7
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-17 01:05:52 +0300

    `Replace hardcoded API URLs with environment variable BASE_URL in api.ts`

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,10 +5,8 @@ import { WishListResponse } from '@/Interfaces/wishlist';
 
 import { AddToWishListResponse, BrandResponse, CategoryResponse, ProductResponse, RemoveFromWishListResponse, SingleBrandResponse, SingleCategoryResponse, SingleProductResponse, SingleSubcategoryResponse, SubcategoryResponse } from "@/types";
 
-
-// class ApiServices {
-
-// }
+// -------------------BASE URL------------------------------
+const BASE_URL = process.env.NEXT_BASE_URL
 // -----------------------------headers-----------------------------------------------------
 
 // headers
@@ -22,7 +20,7 @@ function getHeaders() {
 // -----------------------------------------Products-----------------------------------------------------
 // get All Products
 export async function getAllProducts(page: number = 1): Promise<ProductResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/products?page=${page}`, {
+	return await fetch(`${BASE_URL}v1/products?page=${page}`, {
 		// cache: "force-cache"
 		next: {
 			revalidate: 3600
@@ -33,7 +31,7 @@ export async function getAllProducts(page: number = 1): Promise<ProductResponse>
 // get single product Details
 export async function getSingleProduct(id: string | string[]): Promise<SingleProductResponse> {
 	return await fetch(
-		`https://ecommerce.routemisr.com/api/v1/products/${id}`
+		`${BASE_URL}v1/products/${id}`
 	).then((res) => res.json());
 }
 
@@ -43,7 +41,7 @@ export async function getSingleProduct(id: string | string[]): Promise<SinglePro
 
 // get all brands
 export async function getAllBrands(): Promise<BrandResponse> {
-	return await fetch("https://ecommerce.routemisr.com/api/v1/brands", {
+	return await fetch("${BASE_URL}v1/brands", {
 		cache: "force-cache",
 	})
 		.then((res) => res.json());
@@ -51,14 +49,14 @@ export async function getAllBrands(): Promise<BrandResponse> {
 
 // get all products of the single Brand
 export async function getSingleBrandAllProducts(id: string | string[]): Promise<ProductResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/products?brand=${id}`, {
+	return await fetch(`${BASE_URL}v1/products?brand=${id}`, {
 		cache: "force-cache",
 	}).then((res) => res.json());
 }
 
 // get Single barnd
 export async function getSingleBrand(id: string | string[]): Promise<SingleBrandResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/brands/${id}`, {
+	return await fetch(`${BASE_URL}v1/brands/${id}`, {
 		cache: "force-cache",
 	}).then((res) => res.json());
 }
@@ -67,7 +65,7 @@ export async function getSingleBrand(id: string | string[]): Promise<SingleBrand
 // get all categories
 export async function getAllCategories(): Promise<CategoryResponse> {
 	return await fetch(
-		`https://ecommerce.routemisr.com/api/v1/categories`, {
+		`${BASE_URL}v1/categories`, {
 		cache: "force-cache",
 	}
 	).then((res) => res.json())
@@ -75,13 +73,13 @@ export async function getAllCategories(): Promise<CategoryResponse> {
 
 // get single category 
 export async function getSingleCategory(id: string): Promise<SingleCategoryResponse> {
-	return fetch(`https://ecommerce.routemisr.com/api/v1/categories/${id}`, {
+	return fetch(`${BASE_URL}v1/categories/${id}`, {
 		cache: "force-cache",
 	}).then((res) => res.json());
 }
 // get all products with specific category
 export async function getSingleCategoryAllProducts(id: string): Promise<ProductResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/products?category[in]=${id}`, {
+	return await fetch(`${BASE_URL}v1/products?category[in]=${id}`, {
 		headers: getHeaders()
 	}).then((res) => res.json())
 }
@@ -89,7 +87,7 @@ export async function getSingleCategoryAllProducts(id: string): Promise<ProductR
 // ------------------------------------Subcategories---------------------------------------------------
 // get all subcategories
 export async function getAllSubcategories(): Promise<SubcategoryResponse> {
-	return await fetch("https://ecommerce.routemisr.com/api/v1/subcategories", {
+	return await fetch("${BASE_URL}v1/subcategories", {
 		cache: "force-cache",
 	})
 		.then((res) => res.json());
@@ -97,14 +95,14 @@ export async function getAllSubcategories(): Promise<SubcategoryResponse> {
 
 // get spesific subcategory
 export async function getSingleSubcategory(id: string): Promise<SingleSubcategoryResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/subcategories/${id}`, {
+	return await fetch(`${BASE_URL}v1/subcategories/${id}`, {
 		cache: "force-cache"
 	}).then((res) => res.json())
 }
 
 // get all products with specific subcategory
 export async function getSingleSubCategoryAllProducts(id: string): Promise<ProductResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/products?category[in]=${encodeURIComponent(id)}`, {
+	return await fetch(`${BASE_URL}v1/products?category[in]=${encodeURIComponent(id)}`, {
 		cache: "no-store",
 		headers: getHeaders()
 	}).then((res) => res.json());
@@ -116,7 +114,7 @@ export async function getSingleSubCategoryAllProducts(id: string): Promise<Produ
 // ---------------------------------------Cart-------------------------------------------
 // add products to cart
 export async function addToCart(productId: string | string[]): Promise<AddToCartResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/cart`, {
+	return await fetch(`${BASE_URL}v1/cart`, {
 		method: 'post',
 		body: JSON.stringify({ productId }),
 		headers: getHeaders()
@@ -125,14 +123,14 @@ export async function addToCart(productId: string | string[]): Promise<AddToCart
 
 // get user cart
 export async function getUserCart(): Promise<GetCartResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/cart`, {
+	return await fetch(`${BASE_URL}v1/cart`, {
 		headers: getHeaders()
 	}).then((res) => res.json())
 }
 
 // delete specific cart item
 export async function deleteCartItem(productId: string): Promise<UpdateCartItemResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
+	return await fetch(`${BASE_URL}v1/cart/${productId}`, {
 		method: 'delete',
 		headers: getHeaders()
 	}).then((res) => res.json())
@@ -141,7 +139,7 @@ export async function deleteCartItem(productId: string): Promise<UpdateCartItemR
 // update cart product quantity
 
 export async function updateCartProductQuantity(productId: string, count: number): Promise<UpdateCartItemResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
+	return await fetch(`${BASE_URL}v1/cart/${productId}`, {
 		method: `put`,
 		body: JSON.stringify({ count }),
 		headers: getHeaders()
@@ -149,7 +147,7 @@ export async function updateCartProductQuantity(productId: string, count: number
 }
 // clear cart
 export async function clearCart(): Promise<ClearCartResponse> {
-	return await fetch(`https://ecommerce.routemisr.com/api/v1/cart`, {
+	return await fetch(`${BASE_URL}v1/cart`, {
 		method: `delete`,
 		headers: getHeaders()
 	}).then((res) => res.json())
@@ -159,7 +157,7 @@ export async function clearCart(): Promise<ClearCartResponse> {
 
 // add to wishlist
 export async function addToWishlist(productId: string): Promise<AddToWishListResponse> {
-	const res = await fetch("https://ecommerce.routemisr.com/api/v1/wishlist", {
+	const res = await fetch("${BASE_URL}v1/wishlist", {
 		method: "POST",
 		headers: getHeaders(),
 		body: JSON.stringify({ productId }),
@@ -173,7 +171,7 @@ export async function addToWishlist(productId: string): Promise<AddToWishListRes
 
 // remove from wishlist
 export async function removeFromWishlist(productId: string): Promise<RemoveFromWishListResponse> {
-	const res = await fetch(`https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`, {
+	const res = await fetch(`${BASE_URL}v1/wishlist/${productId}`, {
 		method: "DELETE",
 		headers: getHeaders(),
 		body: JSON.stringify({ productId }),
@@ -187,7 +185,7 @@ export async function removeFromWishlist(productId: string): Promise<RemoveFromW
 // get wishlist
 
 export async function getWishlist(): Promise<WishListResponse> {
-	const res = await fetch(`https://ecommerce.routemisr.com/api/v1/wishlist`, {
+	const res = await fetch(`${BASE_URL}v1/wishlist`, {
 		headers: getHeaders(),
 	});
 	const data: WishListResponse = await res.json();
