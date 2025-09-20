5bd6123bb6fcf7a092bc32ce48c1402a8ccfdc0e
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-09-20 19:26:28 +0300

    `Added SessionProvider to ProvidersContainer`

--- src/components/StoreProvider/ProvidersContainer.tsx
+++ src/components/StoreProvider/ProvidersContainer.tsx
@@ -2,17 +2,20 @@
 import CartContextProvider from '@/Context/CartContext'
 import WishlistContextProvider from '@/Context/WishListContext'
 import { store } from '@/redux/store'
+import { SessionProvider } from 'next-auth/react'
 import React, { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 
 export default function ProvidersContainer({ children }: { children: ReactNode }) {
 	return (
-		<Provider store={store}>
-			<WishlistContextProvider>
-				<CartContextProvider>
-					{children}
-				</CartContextProvider>
-			</WishlistContextProvider>
-		</Provider>
+		<SessionProvider>
+			<Provider store={store}>
+				<WishlistContextProvider>
+					<CartContextProvider>
+						{children}
+					</CartContextProvider>
+				</WishlistContextProvider>
+			</Provider>
+		</SessionProvider>
 	)
 }
