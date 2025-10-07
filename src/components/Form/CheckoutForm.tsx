af892b272607c23b9d239e0480ab6567854ba21b
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-10-07 21:29:41 +0300

    Update CheckoutForm to open checkout URL in a new tab on successful order

--- src/components/Form/CheckoutForm.tsx
+++ src/components/Form/CheckoutForm.tsx
@@ -37,7 +37,7 @@ export default function CheckoutForm({ cartID }: CheckoutFormProps) {
 			const response = await checkoutOrder(cartID, values);
 			console.log(response)
 			if (response.status === "success") {
-				window.open(response.session?.url, "_self")
+				window.open(response.session?.url, "_blank")
 			}
 		} catch (error) {
 			console.log(error)
