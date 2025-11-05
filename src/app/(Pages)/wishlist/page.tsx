8333484f6cd8926764b461a80b7c39802b0c3883
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-11-05 15:12:14 +0200

    refactor: remove handleDisplayWishlist function and pass empty prop to InnerWishList component

--- src/app/(Pages)/wishlist/page.tsx
+++ src/app/(Pages)/wishlist/page.tsx
@@ -1,20 +1,16 @@
-import { getWishlist } from "../../../services/api";
+
 import { Suspense } from "react";
 import LoadingSpinner from "@/components/shared/LoadingSpinner";
 import InnerWishList from "@/components/Wishlist/InnerWishList";
 
 export default async function Page() {
 
-	async function handleDisplayWishlist() {
-		const wishlistRes = await getWishlist();
-		return wishlistRes
-	}
-	const wishListProducts = await handleDisplayWishlist();
+
 
 
 	return (
 		<Suspense fallback={<LoadingSpinner />}>
-			<InnerWishList wishListProducts={wishListProducts} />
+			<InnerWishList />
 		</Suspense>
 	);
 }
